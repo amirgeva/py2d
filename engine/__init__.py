ee2b4af0b3e64cabd21c5e4f2a4207eda67e1992
Amir Geva <amirgeva@gmail.com>
2016-10-11 15:49:41 +0300

    Updated with new functions

--- engine/__init__.py
+++ engine/__init__.py
@@ -11,9 +11,13 @@ from app import Application
 from sprite import Sprite
 from sprite import AnimationSequence
 from sprite import AnimatedSprite
+from sprite import load_xml_file
+from sprite import load_xml_str
+from sprite import load_json_file
+from sprite import load_json_str
 from sprite import load_file
 from sprite import load_str
 from view import View
 
-__all__ = [ 'get_surface','Entity','Scene','RTree','RigidBody','is_transparent','parse_point','parse_color','all_pixels','Application','Sprite','AnimationSequence','AnimatedSprite','load_file','load_str','View' ]
+__all__ = [ 'get_surface','Entity','Scene','RTree','RigidBody','is_transparent','parse_point','parse_color','all_pixels','Application','Sprite','AnimationSequence','AnimatedSprite','load_xml_file','load_xml_str','load_json_file','load_json_str','load_file','load_str','View' ]
 
