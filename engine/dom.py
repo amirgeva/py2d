cbdb95137984f6b5daebc8fee865f7b2e89521ed
Amir Geva <amirgeva@gmail.com>
2016-10-12 23:00:24 +0300

    Added text storage and load returns root element

--- engine/dom.py
+++ engine/dom.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
-from xml.dom import minidom
+from xml.dom import minidom,Node
 
 class Element(object):
     def __init__(self,node):
         self.node=node
+        self.text=''
+        for c in self.node.childNodes:
+            if c.nodeType == Node.TEXT_NODE:
+                self.text=c.nodeValue
 
     def __getitem__(self,name):
         if self.node:
@@ -15,9 +19,11 @@ class Element(object):
         if not self.node:
             return []
         return [Element(e) for e in self.node.getElementsByTagName(tag)]
-
+        
 def parseFile(filename):
-    return Element(minidom.parse(filename))
+    node=minidom.parse(filename)
+    return Element(node.childNodes[0])
 
 def parseString(s):
-    return Element(minidom.parseString(s))
+    node=minidom.parseString(s)
+    return Element(node.childNodes[0])
