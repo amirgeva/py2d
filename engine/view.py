a7a4604694b3dd98463043ce11fb2f2052dc94a6
Amir Geva <amirgeva@gmail.com>
2016-11-12 22:02:08 +0200

    added get_position

--- engine/view.py
+++ engine/view.py
@@ -12,6 +12,9 @@ class View(object):
     def offset(self,d):
         self.rect.move_ip(d[0],d[1])
         
+    def get_position(self):
+        return self.rect.topleft
+        
     def set_position(self,pos):
         self.rect=pygame.Rect(pos.x,pos.y,self.rect.width,self.rect.height)
         
